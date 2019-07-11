99afe0e11adabbc108c08fe846cc0922dbd5c1f8
Raine Wallace <rainebeaudubs@gmail.com>
2019-07-11 13:31:53 -0400

    first draft of users migration

---
+++ data/migrations/20190711131424_Users.js
@@ -0,0 +1,36 @@
+exports.up = function(knex) {
+  return knex.schema.creteTable('Users', tbl => {
+    tbl.increments();
+
+    tbl
+      .integer('firebaseId')
+      .notNullable()
+      .unique();
+
+    tbl
+      .string('firstName', 255)
+      .notNullable();
+
+    tbl
+      .string('lastName', 255)
+      .notNullable();
+
+    tbl
+      .string('email', 255)
+      .notNullable()
+      .unique();
+
+    tbl
+      .string('userPhoto')
+
+    tbl
+      .integer('birthDate')
+
+    tbl
+      .string('status', 255)
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('Users');
+};
