70b4836f6212ec548318926f15d3327f2744f375
pdxdave <webdevdave@hotmail.com>
2019-07-29 12:03:22 -0700

    users-model test

--- testing/tacolog-model.spec.js
+++ testing/tacolog-model.spec.js
@@ -1,40 +1,40 @@
-const db = require('../data/dbConfig');
+// const db = require('../data/dbConfig');
 
-const TacoLogs = require('../data/models/tacolog-model');
+// const TacoLogs = require('../data/models/tacolog-model');
 
-describe('TacoLog Model', () => {
+// describe('TacoLog Model', () => {
 
-    beforeEach(async () => {
-        await db('TacoLog')
-        .truncate();
-    })
+//     beforeEach(async () => {
+//         await db('TacoLog')
+//         .truncate();
+//     })
 
 
-    describe('add()' , () => {
+//     describe('add()' , () => {
 
-        it('will add a tacolog to the db', async () => {
+//         it('will add a tacolog to the db', async () => {
 
-            const tacolog = {
-                date: 1999-12-31,
-                tacoName: "bat taco",
-                tacoLogPhoto: "http://unsplash.it/150/150",
-                restaurantName: "La Grange",
-                address: "678 West Green Street",
-                rating: 3,
-                userExperience: "Superb",
-                notes: "Every day is taco ipsum tuesday. It’s taco Tuesday Monday. Yeah, apparently the taco shack was robbed. They left the money but took the tacos. TACOS!! How bout a gosh darn quesadilla? Burritos are very tasty. Tacos, tacos, tacos. Tacos, tacos, tacos.",
-                reviewedAt: Date.now()
-            }
+//             const tacolog = {
+//                 date: 1999-12-31,
+//                 tacoName: "bat taco",
+//                 tacoLogPhoto: "http://unsplash.it/150/150",
+//                 restaurantName: "La Grange",
+//                 address: "678 West Green Street",
+//                 rating: 3,
+//                 userExperience: "Superb",
+//                 notes: "Every day is taco ipsum tuesday. It’s taco Tuesday Monday. Yeah, apparently the taco shack was robbed. They left the money but took the tacos. TACOS!! How bout a gosh darn quesadilla? Burritos are very tasty. Tacos, tacos, tacos. Tacos, tacos, tacos.",
+//                 reviewedAt: Date.now()
+//             }
 
-            await TacoLogs.add(tacolog)
-            const taco = await db('TacoLog')
-            expect(taco).toHaveLength(1)
+//             await TacoLogs.add(tacolog)
+//             const taco = await db('TacoLog')
+//             expect(taco).toHaveLength(1)
 
-        })  
-    })
+//         })  
+//     })
 
 
 
 
     
-})
+// })
--- testing/users-model.spec.js
+++ testing/users-model.spec.js
@@ -1,141 +1,84 @@
+
 const db = require('../data/dbConfig');
-// const Users = require('../data/models/users-model');
 
-// new stuff
-const {add} = require('../data/models/users-model')
-const request = require('supertest')
-const server = require('../api/server')
+const Users = require('../data/models/users-model');
 
-describe('Users Model', () => {
-
-    beforeEach(async () => {
-        await db('Users')
-        .truncate();
-    })
-
-    describe('Add a new user', () => {
-        it('should insert a new user', async () => {
-            await add ({
-                id: 1,
-                firebaseId: "ThisIsAFakeFirebaseId",
-                firstName: "Jamie",
-                lastName: "Jamison",
-                email: "jamietheman@hotmail.com",
-                userPhoto: "http://unsplash.it/100/100",
-                zipCode: 92392,
-                tacosPerMonth: 5,
-                hardOrSoft: "soft",
-                cornOrFlower: "flower",
-                heatPreference: 5,
-                streetOrGourmet: "street",
-                favTaco: "chicken",
-                favTacoLocation: "Los Cuscos",
-                bestTacoMemory: "homemade tacos",
-                instaHandle: "Instagram goes here",
-                twitterHandle: "twitter goes here",
-                facebookPage: "facebook page goes here",
-                website: "espn.com"
-            })
-
-            const newUser = await db('Users')
-            expect(newUser).toHaveLength(1)
-        })
-
-        it('should output a 404 due to insufficient content', async () => {
-            let newUser = {firstName: "Jamie"}
-
-            const res = await request(server)
-                              .post('/api/users')
-                              .send(newUser)
-            expect(res.statusCode)
-            .toEqual(404)
-        })
+const {user1, user2, user3} = require('./helper.js');
 
 
-        it('should output a 201 success code', async () => {
-            let newUser = {
-                id: 5,
-                firebaseId: "ThisIsAFakeFirebaseId33",
-                firstName: "Jamie",
-                lastName: "Jamison",
-                email: "jamietheman33@hotmail.com",
-                userPhoto: "http://unsplash.it/100/100",
-                zipCode: 92392,
-                tacosPerMonth: 5,
-                hardOrSoft: "soft",
-                cornOrFlower: "flower",
-                heatPreference: 5,
-                streetOrGourmet: "street",
-                favTaco: "chicken",
-                favTacoLocation: "Los Cuscos",
-                bestTacoMemory: "homemade tacos",
-                instaHandle: "Instagram goes here",
-                twitterHandle: "twitter goes here",
-                facebookPage: "facebook page goes here",
-                website: "espn.com"
-            }
+beforeEach(async () => {
+    await db('Users')
+    .truncate()
+})
 
-            const res = await request(server)
-                              .post('/api/users')
-                              .send(newUser)
-            expect(res.statusCode)
-            .toBe(201)
+describe("Users Model", () => {
+  
+    describe("Add a new user", () => {
+
+        // checks to confirm there are no users
+        it('should insert a new user yahoo', async () => {
+            let doesUserExist = await db('Users')
+                    .where({firstName: "Frank"})
+                    expect(doesUserExist).toHaveLength(0)
+
+            // Adds a new user
+            await Users.add( user1 )
+
+            // Test to make sure user exists
+            doesUserExist = await db('Users')
+                    .where({firstName: "Jamie"})
+                    expect(doesUserExist).toHaveLength(1)
+            // Dwezil post confirmed in Insomnia
         })
     })
 
-    describe('Find User By ID', () => {
-        it('should output a 404 if the ID is not found',  () => {
-             request(server)
-            .get('/api/users/31')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(404);
-            // let findId = {id: 21}              CAN REMOVE THIS
-            //                   .get('/api/users/21')
-            //                   .send(findId)
-            // expect(res.statusCode)
-            // .toBe(404)
+    describe("Find all Users", () => {
+
+        // passing two new users
+        it('should return all Users', async () => {
+            await Users.add(user1)
+            await Users.add(user2)
+
+            let allUsers = await Users.find()
+            expect(allUsers).toHaveLength(2)
+            // David Lee Roth and Edward Van Halen posts confirmed in Insomnia
         })
+    }) 
+
+    describe("Delete a User by id", () => {
+
+        // passing two new users, then the second one will be deleted
+        it('should return all Users', async () => {
+            await Users.add(user1)
+            await Users.add(user2)
+
+        let delUser = await Users.removeUser(1)
+        expect(delUser).toBe(1)
+        // David Lee Roth post confirmed in Insomnia
     })
+  }) 
 
-    describe('Find User By ID', () => {
-        it('should output a 200 if successful', () => {
-            let Id = 2
-            request(server)
-            .get(`/api/users/${Id}`)
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200);
+  describe("Find a user by id", () => {
+    // passing three new users, then the second one will be the target id
+    it('should return all users', async () => {
+      await Users.add(user1)
+      const targetUser = await Users.add(user2)
+      await Users.add(user3)
 
-            // const res = await request(server)   CAN REMOVE THIS
-            //                   .get('/api/users/2')
-            //                   .send(findID)
-            // expect(res.statusCode)
-            // .toBe(200)
-            
-        })
-    })
+      
+      await Users.findById(2)
+      expect(targetUser.firstName).toBe('Bob')
+  })
+}) 
 
-    describe('Find User By ID', () => {
-        it('should output a 500 if server error', () => {
-            request(server)
-            .get('/api/users/')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(500);
-
-            // let findID = {id: 2}         CAN REMOVE THIS
-
-            // const res = await request(server)
-            //                   .get('/api/users/2')
-            //                   .send(findID)
-            // expect(res.statusCode)
-            // .toBe(200)
-           
-        })
-    })
-
-
-
-
+describe("Update a user by id", () => {
+    // passing a new user
+    it('should return an updated user ', async () => {
+      // create an initial user
+      const newUser = await Users.add(user1)
+      await Users.update(newUser.id, user2)
+      const updatedUser = await Users.findById(newUser.id)
+      expect(updatedUser.firstName).toBe("Bob")
+  })
+ })  
 })
