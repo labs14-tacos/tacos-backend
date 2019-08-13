5b23e6488a77d630cd0569c9c6402b94bb929a8f
meginks <meganjones.dev@gmail.com>
2019-08-13 17:31:25 -0500

    fixed taco log routes

--- routers/userprofile-router.js
+++ routers/userprofile-router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Users = require('../data/models/users-model');
+const TacoLogs = require('../data/models/tacolog-model');
 const {decodeHeader} = require('../authentication/auth-middleware')
 
 
@@ -13,5 +14,25 @@ router.get('/my_info', decodeHeader, async (req, res) => {
       }) )
   })
   
+  
+// GET Tacologs by User who is Logged In 
+
+router.get('/mytacolog', decodeHeader, async (req, res) => {
+  const firebaseId = req.headers.user.firebaseId
+  try {
+    const test = await TacoLogs.findUserTacosById(firebaseId)
+    if(!test){
+      res.status(404).json({
+        message: "No taco logs belonging to this user could not be found"
+      })
+    } else {
+      res.status(200).json(test)
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "There was a server error"
+    })
+  }
+})
 
   module.exports = router;
